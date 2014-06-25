af7e1e206be63f2f92e4f57be8d6762eac3cc652
IsaacM <isaacm@users.noreply.github.com>
2014-06-25 13:40:13 -0400

    add btree test

---
+++ isbtree.clj
@@ -0,0 +1,7 @@
+(fn is-btree? [[rt lc rc :as x]]
+  (cond
+    (some false? x) false
+      (= 3 (count x)) (and (if (coll? lc) (is-btree? lc) true)
+                      (if (coll? rc) (is-btree? rc) true)
+                      (not (or (nil? rt) (coll? rt))))
+      :else false))
